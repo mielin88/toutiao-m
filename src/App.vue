8f2ab651c56f5bd5f7c1cb6c3d33cc280550c529
Mr.meilin <jimeilin11@163.com>
2020-05-12 11:24:23 +0800

    项目初始化-引入Vent组件库

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,13 @@
     <!-- 路由占位符 -->
     <router-view/>
     <h1>黑马头条</h1>
+    <i class="toutiao toutiao-wuwangluo"></i>
+
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
   </div>
 </template>
 
